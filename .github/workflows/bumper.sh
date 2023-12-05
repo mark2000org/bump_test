936f90011042d47e9d14b5802419b1b080f55e32
Mark Stephenson <mark2000stephenson@gmail.com>
2023-12-05 13:52:27 -0700

    bumper

---
+++ .github/workflows/bumper.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+
+regex='([0-9]+\.[0-9]+\.[0-9]+b)([0-9]+)'
+
+# Read file line by line and process each line
+while IFS= read -r version || [[ -n "$version" ]]; do
+    if [[ $version =~ $regex ]]; then
+        # Extract the last number and increment it by one
+        last_number=${BASH_REMATCH[2]}
+        incremented_number=$((last_number + 1))
+        # Replace the last number in the line
+        updated_version=${BASH_REMATCH[1]}$incremented_number
+        echo "Bumping from $version to $updated_version"
+    else
+        echo "Version $version is not a beta version; not updating version number"
+        exit 0
+    fi
+done < $1
+
+echo "$updated_version" > $1.tmp
+mv $1.tmp $1
+rm $1.tmp
